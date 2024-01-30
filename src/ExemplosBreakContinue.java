c6069eff495897f50ee324892e15f452f36a0574
thgCSC <thyagoshadows@gmail.com>
2024-01-29 21:44:53 -0300

    Exemplo com break e continue com FOR

--- src/ExemplosBreakContinue.java
+++ src/ExemplosBreakContinue.java
@@ -2,7 +2,8 @@ public class ExemplosBreakContinue {
     public static void main(String[] args) {
         for (int numero = 1; numero <= 5; numero++) {
             if (numero == 3)
-                break;
+                // break;
+                continue;
             System.out.println(numero);
         }
     }
