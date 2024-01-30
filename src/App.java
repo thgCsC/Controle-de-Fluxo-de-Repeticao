e4de5d2dfd3074b360e4f178de2048d0ec230071
thgCSC <thyagoshadows@gmail.com>
2024-01-29 22:47:34 -0300

    Exercicios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*
+         * int numero = 1;
+         * for (int x = 1; x < 2; x++) {
+         * numero = numero + x;
+         * }
+         */
+        /*
+         * int num = 5, count = 1;
+         * do {
+         * num += count;
+         * System.out.println(num);
+         * } while (count <= 3);
+         */
+        for (int x = 1; x <= 10; x++) {
+
+            if (x % 2 == 0) // % operador que representa módulo de uma divisão
+
+                System.out.println(x);
+
+        }
+
     }
 }
