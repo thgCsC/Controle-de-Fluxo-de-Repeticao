d8cc693d11ce7c7c8af480e659253afa1169d6af
thgCSC <thyagoshadows@gmail.com>
2024-01-29 21:38:55 -0300

    Exemplo de For em Arrays...

--- src/ExemploFor.java
+++ src/ExemploFor.java
@@ -1,5 +1,8 @@
 public class ExemploFor {
     public static void main(String[] args) {
-
+        for (int carneirinhos = 0; carneirinhos <= 20; carneirinhos++) {
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+        System.out.println("E joãozinho dormiu... ZzZzZ");
     }
 }
---
+++ src/ForEmArrays.java
@@ -0,0 +1,9 @@
+public class ForEmArrays {
+    public static void main(String[] args) {
+        String alunos[] = { "Felipe", "Jonas", "Julia", "Marcos" };
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no indice x = " + i + " é " + alunos[i]);
+        }
+    }
+}
